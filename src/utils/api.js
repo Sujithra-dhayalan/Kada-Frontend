4ba8e1d86df1786de122fd3657e734c1db832dd6
Sujithra-dhayalan <sujithradhayalan@gmail.com>
2025-12-14 17:56:51 +0530

    added url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Matches server PORT in `.env` (default 5000)
+  baseURL: `${import.meta.env.VITE_API_URL}`,
 });
+
 
 // Interceptor: Attach Token to every request automatically
 api.interceptors.request.use((config) => {
